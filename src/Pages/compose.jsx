0018fa70d6361bb08510a9be4a95ea4f7cf3493c
thomaspfresco <thomasfresco@live.com>
2024-07-13 23:52:51 +0100

    fix

--- src/Pages/compose.jsx
+++ src/Pages/compose.jsx
@@ -13,9 +13,6 @@ import axios from 'axios';
 
 function Compose() {
 
-  const [dataOla, setDataOla] = useState('');
-  const [dataAdeus, setDataAdeus] = useState('');
-
   const [loading, setLoading] = useState(false); // Initialize loading state to true
   const [isMobileDevice, setIsMobileDevice] = useState(false);
 
@@ -33,36 +30,12 @@ function Compose() {
       setLoading(false); // Set loading state to false when myFunction is done
     }, 200); // Adjust the delay as needed
   };*/
-  
-  const loadSession = async () => {
-    setLoading(true); 
-    try {
-
-      const token = window.localStorage.getItem("token");
-      
-      const response = await axios.get(window.serverLink+"/load", {
-        params: {
-          token: token
-        }
-      });
-
-      setLoading(false);
-      return response.data.session;
-      
-    } catch (error) {
-      console.error('Error loading session:', error);
-    }
-  };
 
   useEffect(() => {
     const startAudioContext = () => { Tone.start(); };
     document.addEventListener('click', startAudioContext, { once: true });
     return () => { document.removeEventListener('click', startAudioContext); };
   }, []);
-
-  useEffect(() => {
-    setIsMobileDevice(isMobile);
-  }, [isMobileDevice]); 
 
   /*useEffect(() => {
     const handleBeforeUnload = (event) => { saveProject(p5InstanceRef.current.project); };
@@ -72,39 +45,16 @@ function Compose() {
   
   useEffect(() => {
 
-    if (!loading && !p5InstanceRef.current) {
+    if (!p5InstanceRef.current) {
       p5InstanceRef.current = new p5(sketch(setLoading), p5ContainerRef.current);
     }
 
   }, []); // Only run this effect when loading changes
 
-  useEffect(() => {
-    // Function to handle resizing
-    const handleResize = () => {
-      if (p5InstanceRef.current) {
-        // Update canvas size to match window size
-        const { innerWidth, innerHeight } = window;
-        p5InstanceRef.current.resizeCanvas(innerWidth, innerHeight);
-        // If you have a method for responsiveness, you can call it here
-        // For example: p5InstanceRef.current.getResponsive();
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-    handleResize();
-  
-    // Cleanup function to remove event listener when component unmounts
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []); // Empty dependency array to run effect only once on mount
-
   return (
     <div className="canvas">
       <div ref={p5ContainerRef}></div>
       <a id="export"></a>
-      {loading ? <Loading /> : null}
-      {isMobileDevice ? <MobileMsg /> : null}
     </div>
   );
 
